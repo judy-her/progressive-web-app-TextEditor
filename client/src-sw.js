62ec7db3a280264cd57edb1259f19e9ce297cdf5
judy_her <judith85dm@ucla.edu>
2024-04-03 17:19:22 -0700

    added cache to register route

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,30 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+offlineFallback({
+  pageFallback: '/offline.html', // Path to your offline fallback page
+  strategy: pageCache,
+});
 registerRoute();
+
+registerRoute(
+  // Define the pattern for asset requests
+  ({ request }) =>
+    request.destination === 'style' ||
+    request.destination === 'script' ||
+    request.destination === 'image',
+  // Use the CacheFirst strategy for asset caching
+  new CacheFirst({
+    cacheName: 'assets-cache',
+    plugins: [
+      // Cache responses with status 0 and 200
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      // Set an expiration for cached assets
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+      }),
+    ],
+  })
+);
